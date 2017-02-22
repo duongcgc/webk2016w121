c3aa70d212e256ae2a2762215efc27c538709350
P03-M01-PC\P03-M01 <duongcgc@gmail.com>
2017-02-22 21:03:40 +0700

    lession 8 eache and mixin in sass

---
+++ Lesson08/color.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <span class="block back_color_01 text_color_01">FFC000</span>
+    <span class="block back_color_02 text_color_01">363636</span>
+    <span class="block back_color_03 text_color_01">808080</span>
+    <span class="block back_color_04 text_color_01">b7b7b7</span>
+    <span class="block back_color_05 text_color_01">d7d7d7</span>
+    <span class="block back_color_06 text_color_02">f4f4f4</span>
+    
+</body>
+</html>
