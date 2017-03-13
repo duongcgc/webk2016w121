8e707e589e9616ab0716702e05a92ba38077bb65
P03-M01-PC\P03-M01 <duongcgc@gmail.com>
2017-03-13 20:57:39 +0700

    jquery lession 03

---
+++ jQuery-Projects/Lession03-jquery/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('h1').click(function(){
+            alert("Thanks you for click, My name JQuery");
+            $('h1').text("Just Clicked!");
+    });        
+});
